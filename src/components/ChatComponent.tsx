6f04c7d3991752f47fb542ef4117d84adc3adb03
Mark Struck <markstruck@comcast.net>
2023-09-02 03:11:57 -0500

    updating styling of the ChatComponent

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -6,44 +6,52 @@ const llm = new OpenAI({
   temperature: 0.9,
 });
 
+type Message = {
+  text: string;
+  sender: string;
+};
+
 const ChatComponent = () => {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState<Array<Message>>([]);
   const [input, setInput] = useState('');
 
   const handleSend = () => {
     if (input.trim() !== '') {
-        const result = llm.predict(input)
+      messages.push({ text: input, sender: 'You' });
+      setMessages([...messages,] );
+      llm.predict(input)
         .then(res => {
           console.log(res);
-          messages.push({ text: input, sender: 'You' });
-          messages.push({ text: res, sender: 'Bot' });
-          setMessages([...messages, ]);
+          messages.push({ text: res, sender: 'ChatGPT' })
+          setMessages([...messages,] );
           setInput('');
         });
     }
   };
 
   return (
-    <div style={{ padding: '20px', border: '1px solid #ccc', width: '400px' }}>
-      <div style={{ height: '300px', overflowY: 'scroll' }}>
-        {messages.map((message, index) => (
-          <div key={index}>
-            <strong>{message.sender}:</strong> {message.text}
-          </div>
-        ))}
-      </div>
-      <div style={{ marginTop: '20px' }}>
+    <>
+      <div style={{ marginTop: '20px', display: 'flex' }}>
         <input
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          style={{ width: '80%' }}
+          style={{ width: '75%', fontSize: '16px', paddingLeft: '10px', backgroundColor: 'rgba(64,65,79)' }}
         />
-        <button onClick={handleSend} style={{ width: '18%', marginLeft: '2%' }}>
+        <button type="submit" onClick={handleSend} style={{ width: '18%', marginLeft: '2%', backgroundColor: 'grey' }}>
           Send
         </button>
       </div>
-    </div>
+      <div style={{ marginTop: '20px', padding: '20px', border: '1px solid #ccc', width: '400px', backgroundColor: 'rgba(52,53,65)' }}>
+        <div style={{ height: '300px', overflowY: 'scroll' }}>
+          {messages.map((message, index) => (
+            <div key={index} style={{ textAlign: 'left', lineHeight: '30px' }}>
+              <strong>{message.sender}:</strong> {message.text}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
